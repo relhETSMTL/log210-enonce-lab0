c6be3afa91bbdeb1fedf5b09db5c9d30a902e1d6
Yvan Ross <Yvan.ross@gmail.com>
2020-10-11 14:29:53 -0400

    completed action for test

--- .github/actions/text-exist/entrypoint.sh
+++ .github/actions/text-exist/entrypoint.sh
@@ -1,23 +1,30 @@
 #!/bin/sh -l
 
-echo "text-exist $1 > $2 > $3"
+echo "text-exist $1 > $3"
+echo " ">> $3
+echo " "
+if [  -f $1 ]; then
+  echo "EXIST => $1 true" >> $3
+  echo "EXIST => $1 true" 
+else
 
+  echo "EXIST => $1 false" >> $3
+  echo "EXIST => $1 false" 
+fi
 
-mails=$(echo $2 | tr " " "_" | tr ";" "\n")
-for addr in $mails
+file_content=$(cat $1 | tr -d "[:space:]" | tr "-" "!")
+phrases=$(echo $2 | tr " " "_" | tr ";" "\n")
+
+for line in $phrases
 do
-  res=$(echo $addr | tr "_" " ")
-  present=$(grep -F "$res"  $1 | wc -l)
-  # echo ::set-output name=present::$present
-  if [ $present = "1" ];  then
-    echo "SUCCESS => $1 [$res] is present" >> $3
-    echo "SUCCESS => $1 [$res] is present "
+  line_with_space=$(echo $line | tr "_" " ")
+  phrase=$(echo $line | tr "_" " " | tr -d "[:space:]" | tr "-" "!")
+  nb_lines=$(echo $file_content | grep -- $phrase | wc -l)
+  if [ $nb_lines = "1" ];  then
+    echo "SUCCESS => $1 [$line_with_space] text-exist" >> $3
+    echo "SUCCESS => $1 [$line_with_space] text-exist"
   else
-    echo "ERROR => $1 [$res] is not present" >> $3
-    echo "ERROR =>$1  [$res] is not present" 
+    echo "ERROR => $1 [$line_with_space] not text-exist" >> $3
+    echo "ERROR =>$1  [$line_with_space] not text-exist" 
   fi
-
 done
-
-
-       
