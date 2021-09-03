052140eeccfb5ebc1b175b2f213b4e8c6ad25e40
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-09-03 14:32:23 -0400

    tests index (redemarrer) et classement

--- test/views/index-lab0.test.ts
+++ test/views/index-lab0.test.ts
@@ -2,16 +2,29 @@ import 'jest-extended';
 import { readFileSync } from 'fs';
 const path = require('path');
 
-let content = ""
-beforeAll(async () => {
-  const filename = path.join('views', 'index.pug')
-  content = readFileSync(filename, 'utf-8');
-});
+const indexPugFilename = path.join('views', 'index.pug')
+const contentIndex = readFileSync(indexPugFilename, 'utf-8');
+const classementPugFilename = path.join('views', 'classement.pug')
+const contentClassement = readFileSync(classementPugFilename, 'utf-8');
 
 describe('views/index.pug', () => {
   it("should contain button.redemarrer Redémarrer", async () => {
-    expect((content.indexOf("button#redemarrer Redémarrer") > -1) ||
-    (content.indexOf("button.redemarrer Redémarrer") > -1)).toBeTrue();
+    expect((contentIndex.indexOf("button#redemarrer Redémarrer") > -1) ||
+    (contentIndex.indexOf("button.redemarrer Redémarrer") > -1)).toBeTrue();
+  });
+
+  it("should contain button.classement Classement", async () => {
+    expect((contentIndex.indexOf("button#classement Classement") > -1) ||
+    (contentIndex.indexOf("button.classement Classement") > -1)).toBeTrue();
+  });
+
+});
+
+describe('views/classement.pug', () => {
+
+  it("should contain button.home Accueil", async () => {
+    expect((contentClassement.indexOf("button#home Accueil") > -1) ||
+    (contentClassement.indexOf("button.home Accueil") > -1)).toBeTrue();
   });
 
 });
