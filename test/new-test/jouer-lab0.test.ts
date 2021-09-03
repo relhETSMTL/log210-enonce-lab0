006aa2784cfeeb4738090499d01c9a4c0ae51918
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-09-03 10:32:03 -0400

    meilleur message describe

--- test/new-test/jouer-lab0.test.ts
+++ test/new-test/jouer-lab0.test.ts
@@ -8,7 +8,7 @@ beforeAll(async () => {
   content = readFileSync(filename, 'utf-8');
 });
 
-describe('test/jouer.test.ts', () => {
+describe('test/routes/jeuRouter-jouer.test.ts', () => {
   it("should contain expect(response.body.resultat.v3).toBeWithin(1, 7)", async () => {
     expect(content).toInclude("expect(resultat.v3).toBeWithin(1, 7)");
   });
