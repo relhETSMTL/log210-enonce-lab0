f27c4ea95258c8e104bb10d9f2f3802de4b506ff
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-09-03 16:02:01 -0400

    Tester la solution proposée dans le Readme

--- test/views/index-lab0.test.ts
+++ test/views/index-lab0.test.ts
@@ -8,14 +8,12 @@ const classementPugFilename = path.join('views', 'classement.pug')
 const contentClassement = readFileSync(classementPugFilename, 'utf-8');
 
 describe('views/index.pug', () => {
-  it("should contain button.redemarrer Redémarrer", async () => {
-    expect((contentIndex.indexOf("button#redemarrer Redémarrer") > -1) ||
-    (contentIndex.indexOf("button.redemarrer Redémarrer") > -1)).toBeTrue();
+  it("should contain button#redemarrer Redémarrer", async () => {
+    expect(contentIndex).toInclude("button#redemarrer Redémarrer");
   });
 
-  it("should contain button.classement Classement", async () => {
-    expect((contentIndex.indexOf("button#classement Classement") > -1) ||
-    (contentIndex.indexOf("button.classement Classement") > -1)).toBeTrue();
+  it("should contain button#classement Classement", async () => {
+    expect(contentIndex).toInclude("button#classement Classement");
   });
 
 });
@@ -23,8 +21,7 @@ describe('views/index.pug', () => {
 describe('views/classement.pug', () => {
 
   it("should contain button.home Accueil", async () => {
-    expect((contentClassement.indexOf("button#home Accueil") > -1) ||
-    (contentClassement.indexOf("button.home Accueil") > -1)).toBeTrue();
+    expect(contentClassement).toInclude("button#home Accueil");
   });
 
 });
