5135292a339726ea935b23fcb724aa6ce797c557
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-01-09 21:54:41 -0500

    Meilleure vérification du fichier test

--- test/new-test/redemarrerJeu-lab0.test.ts
+++ test/new-test/redemarrerJeu-lab0.test.ts
@@ -9,11 +9,12 @@ beforeAll(async () => {
 });
 
 describe('redemarrerJeu.test.ts', () => {
-  it("should contain GET /api/v1/jeu/redemarrerJeu", () => {
-    expect(content.includes("GET /api/v1/jeu/redemarrerJeu")).toBeTruthy();
+  it("should contain \"get('/api/v1/jeu/redemarrerJeu')\"", () => {
+    expect(content.includes("get('/api/v1/jeu/redemarrerJeu')")).toBeTruthy();
   });
 
-  it("should contain Call responds with bad request when players do not exist", () => {
-    expect(content.includes("Call responds with bad request when players do not exist")).toBeTruthy();
+  it("devrait contenir un test pour jouer qui retourne 404 (après redemarrerJeu()", () => {
+    expect(content.includes("/api/v1/jeu/jouer/")).toBeTruthy();
+    expect(content.includes(".status).toBe(404)")).toBeTruthy();
   });
 });
